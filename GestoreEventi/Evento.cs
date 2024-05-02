2511dc4e5eacd0b6f71a70b2feec6edf102cf50c
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-05-02 12:49:31 +0200

    Millestone 2 fatto

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace GestoreEventi
+{
+    public class Evento
+    {
+        private string _titolo;
+        private DateTime _data;
+        private int _capienzaMassima;
+        private int _numeroPostiPrenotati;
+
+        public string Titolo 
+        { 
+            get 
+            { 
+                return _titolo; 
+            } 
+            set 
+            { 
+                if (value.Length <= 1) 
+                    throw new Exception("Il titolo non può essere vuoto"); 
+                _titolo = value; 
+            } 
+        }
+        public int CapienzaMassima 
+        { get 
+            { 
+                return _capienzaMassima; 
+            } 
+            private set 
+            { 
+                if (value <= 0) 
+                    throw new Exception("La capienza dell'evento non puo essere negativa o 0"); 
+                else _capienzaMassima = value; 
+            } 
+        }
+
+        public string Data
+        {
+            get
+            {
+                return _data.ToString("dd/MM/yyyy");
+            }
+            set
+            {
+                if (DateTime.TryParseExact(value, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out _data)) ;
+            }
+        }
+        public int NumeroPostiPrenotati 
+        { 
+            get 
+            { 
+                return _numeroPostiPrenotati; 
+            } 
+            private set 
+            { 
+                if (value < 0) 
+                    throw new Exception("Il numero di posti da riservare non puo essere negativo o 0"); 
+                else 
+                    _numeroPostiPrenotati = value; 
+            } 
+        }
+
+        public Evento(string titolo, string dataInput, int capienza)
+        {
+            
+            Data = dataInput;
+            Titolo = titolo;
+            CapienzaMassima = capienza;
+            NumeroPostiPrenotati = 0;
+        }
+
+        public void PrenotaPosti(int posti)
+        {
+            if(posti > _capienzaMassima)
+            {
+                throw new Exception("Non puoi prenotare più posti oltre la capienza massima");
+            }
+            if(_data <  DateTime.Now)
+            {
+                throw new Exception("Non puoi prenotare dei posti per un evento già passato");
+            }
+            _numeroPostiPrenotati += posti;
+            _capienzaMassima -= posti;
+        }
+        public void DisdiciPosti(int posti)
+        {
+            if(posti > _numeroPostiPrenotati)
+            {
+                throw new Exception("Non puoi disdire più posti di quelli prenotati");
+            }
+            if(posti <= 0)
+            {
+                throw new Exception("Non puoi disdire numero di posti negativo o 0");
+            }
+            if(_data < DateTime.Now)
+            {
+                throw new Exception("Non puoi disdire dei posti per un evento già passato");
+            }
+            _capienzaMassima += posti;
+            _numeroPostiPrenotati -= posti;
+        }
+
+        public override string ToString()
+        {
+            return $"{_data.ToString("dd/MM/yyyy")} - {_titolo}";
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("Gestore Eventi v1.0");
+            Console.Write("Inserire il nome dell'evento: ");
+            string titolo = Console.ReadLine();
+            Console.WriteLine("");
+            Console.Write("Inserire la data dell'evento (gg/mm/yyyy): ");
+            string dataInput = Console.ReadLine();
+            Console.WriteLine("");
+            Console.Write("Inserire il numero di posti totali: ");
+            int capienza = int.Parse(Console.ReadLine());
+
+            Evento e = new(titolo, dataInput, capienza);
+            Console.WriteLine("");
+            Console.Write("Quanti posti desideri prenotare? ");
+            int posti = int.Parse(Console.ReadLine());
+            e.PrenotaPosti(posti);
+
+
+
+                
+
+ 
+
+
+            bool richiesta = true;
+            while (richiesta)
+            {
+                Console.Write("Vuoi disdire dei posti (si/no)? "); 
+                string useChoice = Console.ReadLine();
+                if(useChoice == "si")
+                {
+                    try
+                    {
+                        Console.Write("Indicare il numero dei posti da disdire: ");
+                        int postiDaDisdire = int.Parse(Console.ReadLine());
+                        e.DisdiciPosti(postiDaDisdire);
+                        Console.WriteLine($"Numero di posti prenotati = {e.NumeroPostiPrenotati}");
+                        Console.WriteLine($"Numero di posti disponibili = {e.CapienzaMassima}");
+                    }
+                    catch (Exception s)
+                    {
+                        Console.WriteLine(s.Message);
+                    }
+
+                }
+                else if(useChoice == "no")
+                {
+                    Console.WriteLine("Va bene!");
+                    Console.WriteLine($"Numero di posti prenotati = {e.NumeroPostiPrenotati}");
+                    Console.WriteLine($"Numero di posti disponibili = {e.CapienzaMassima}");
+                    richiesta =false;
+                }
+                
+            }
         }
     }
 }
