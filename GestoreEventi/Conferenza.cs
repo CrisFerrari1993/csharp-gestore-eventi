89f264bcb9c22424a171ec9cf1bfd4b939b23403
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-05-02 18:20:31 +0200

    Consegna con bonus

---
+++ GestoreEventi/Conferenza.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Conferenza : Evento
+    {
+        private string _relatore;
+        private double _prezzo;
+        public string Relatore
+        {
+            get
+            {
+                return _relatore;
+            }
+            set
+            {
+                if (value.Length <= 1)
+                    throw new Exception("Il nome del relatore non puo essere vuoto o con un carattere");
+                else
+                    _relatore = value;
+            }
+        }
+        public double Prezzo { 
+            get
+            { 
+                return _prezzo;
+            } 
+            set 
+            {
+                if (value < 0)
+                    throw new Exception("Il prezzo non può essere negativo");
+                else 
+                    _prezzo = value;
+            } 
+        }
+        public Conferenza(string titolo, string dataInput, int capienza, string relatore, double prezzo) : base(titolo, dataInput, capienza)
+        {
+            _relatore = relatore;
+            Prezzo = prezzo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Data} - {Titolo} - {Relatore} - {Prezzo.ToString("0.00")} euro"; ;
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -56,19 +56,25 @@ namespace GestoreEventi
             //   }
 
             //}
-            // Millestone 3 && 4
+            // Millestone 3 && 4 && bonus
+
+            //stampo versione app
             Console.WriteLine("Gestore Eventi v1.0");
+            //Setto var per controllare ciclo while
             bool programmaInFunzione = true;
             while (programmaInFunzione)
             {
                 try
                 {
+                    //Prova di inserimento dei dati (Se errati, ritenta l'inserimento)
                     Console.Write("Inserisci il nome del tuo programma Eventi: ");
                     string titolo = Console.ReadLine();
                     ProgrammaEventi programmaEventi = new(titolo);
                     Console.Write("Indica il numero di eventi da inserire: ");
                     int numeroEventi = int.Parse(Console.ReadLine());
                     Console.WriteLine("");
+
+                    //Ciclo per inserire gli eventi in base al numero di eventi fornito
                     for (int i = 0; i < numeroEventi; i++)
                     {
                         try
@@ -80,19 +86,19 @@ namespace GestoreEventi
                             Console.Write("Inserisci il numero di posti totali: ");
                             int postiTotali = int.Parse(Console.ReadLine());
                             Console.WriteLine("");
-
-
                             Evento e = new(titoloEvento, dataEvento, postiTotali);
                             programmaEventi.AggiungiEvento(e);
 
-                        } catch (Exception ex)
+                        }
+                        catch (Exception ex)
                         {
+                            //In caso di incoerenza dei dati, il sistema cerca di reinserirli in modo corretto facendo ricompilare i campi all'utente
                             bool erroreCreato = true;
                             Console.WriteLine($"Errore: " + ex.Message + " all'evento:  " + (i + 1));
                             string titoloEvento = "";
                             string dataEvento = "";
                             int postiTotali = 0;
-                            while (erroreCreato) 
+                            while (erroreCreato)
                             {
                                 Console.Write($"Inserisci il nome del {i + 1}° evento: ");
                                 titoloEvento = Console.ReadLine();
@@ -101,9 +107,10 @@ namespace GestoreEventi
                                 Console.Write("Inserisci il numero di posti totali: ");
                                 postiTotali = int.Parse(Console.ReadLine());
                                 Console.WriteLine("");
-                                
+
                                 Evento e = new(titoloEvento, dataEvento, postiTotali);
                                 programmaEventi.AggiungiEvento(e);
+                                //in caso di successo, esci dal ciclo
                                 erroreCreato = false;
 
                             }
@@ -111,29 +118,81 @@ namespace GestoreEventi
 
                         }
                     }
-
+                    //stampo quanti eventi sono presenti attualmente nella lista degli eventi
                     Console.WriteLine($"Il numero di eventi nel programma è: {programmaEventi.StampaEventi()}");
                     Console.WriteLine("Ecco il tuo programma eventi: ");
-                
+                    //assegno alla variabile una funzione che ritorna una stringa di eventi concatenati al nome della Lista degli eventi e tutti gli eventi
                     string listaEventi = programmaEventi.CostruisciStringaEventi(programmaEventi.ListaEventi);
+                    // stampo il risultato
                     Console.WriteLine(listaEventi);
 
                     Console.WriteLine("");
+
                     Console.Write("Inserisci una data per sapere che eventi ci saranno in programma(gg-mm-yyyy): ");
                     string userData = Console.ReadLine();
+                    //creo lista che come valore contiene una lista di eventi che hanno la stessa data della ricerca
                     List<Evento> eventiInData = programmaEventi.EventiPresenti(userData);
-                    foreach(var e in eventiInData)
+                    // e stampo gli eventi
+                    foreach (var e in eventiInData)
                     {
                         Console.WriteLine(e.ToString());
                     }
+                    //BONUS
+                    Console.WriteLine("");
+                    Console.WriteLine("----- BONUS -----");
+                    Console.WriteLine("");
+                    Console.WriteLine("Aggiungiamo anche una conferenza!");
+                    try
+                    {
+                        Console.Write("Inserisci il nome della conferenza: ");
+                        string nomeConferenza = Console.ReadLine();
+                        Console.Write("Inserisci la data della conferenza (gg/mm/yyyy): ");
+                        string dataConferenza = Console.ReadLine();
+                        Console.Write("Inserisci il numero di posti della conferenza: ");
+                        int posti = int.Parse(Console.ReadLine());
+                        Console.Write("Inserisci il relatore (Nome, Cognome) della conferenza: ");
+                        string nomeRelatore = Console.ReadLine();
+                        Console.Write("Inserisci il prezzo del biglietto della conferenza: ");
+                        double prezzoConferenza = double.Parse(Console.ReadLine());
+                        programmaEventi.AggiungiEvento(new Conferenza(nomeConferenza, dataConferenza, posti, nomeRelatore, prezzoConferenza));
 
-                    programmaEventi.SvuotaLista();
+                    }
+                    catch (Exception e)
+                    {
+                        bool erroreCreato = true;
+                        Console.WriteLine($"Errore: " + e.Message);
+                        string nomeConferenza = "";
+                        string dataConferenza = "";
+                        int posti = 0;
+                        string nomeRelatore = "";
+                        double prezzoConferenza = 0;
+                        while (erroreCreato)
+                        {
+                            Console.WriteLine("Errore: " + e.Message);
+                            Console.Write("Inserisci il nome della conferenza: ");
+                            nomeConferenza = Console.ReadLine();
+                            Console.Write("Inserisci la data della conferenza (gg/mm/yyyy): ");
+                            dataConferenza = Console.ReadLine();
+                            Console.Write("Inserisci il numero di posti della conferenza: ");
+                            posti = int.Parse(Console.ReadLine());
+                            Console.Write("Inserisci il relatore (Nome, Cognome) della conferenza: ");
+                            nomeRelatore = Console.ReadLine();
+                            Console.Write("Inserisci il prezzo del biglietto della conferenza: ");
+                            prezzoConferenza = double.Parse(Console.ReadLine());
+                            programmaEventi.AggiungiEvento(new Conferenza(nomeConferenza, dataConferenza, posti, nomeRelatore, prezzoConferenza));
+                            erroreCreato = false;
+                        }
+
+                    }
+                    listaEventi = programmaEventi.CostruisciStringaEventi(programmaEventi.ListaEventi);
+                    Console.WriteLine(listaEventi.ToString());
                     programmaInFunzione = false;
-                } catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
                 }
-
+                catch (Exception e)
+                {
+                    Console.WriteLine("Errore: " + e.Message);
+                }
+                
             }
         }
     }
